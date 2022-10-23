c5326d1d83cf1ba5e960e3b67c8a4ec8430b6f08
Isaac Leung <isaacleung@cmail.carleton.ca>
2022-10-22 21:00:25 -0400

    Bootstrap header (#5)
    * Basic Navbar Example
    
    * Updated Instructor Header

--- web-quiz-app/src/App.js
+++ web-quiz-app/src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import HeaderInstructor from "./components/HeaderInstructor";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     
     <div className="App">
-      <div className="App2">
-        <HeaderInstructor></HeaderInstructor>
-      </div>
+	  <div className="Header">
+	    <HeaderInstructor></HeaderInstructor>
+	  </div>
       
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
--- web-quiz-app/src/components/HeaderInstructor.jsx
+++ web-quiz-app/src/components/HeaderInstructor.jsx
@@ -1,12 +1,34 @@
-
-
+import Nav from 'react-bootstrap/Nav';
 function HeaderInstructor() {
-    return (
-        <div class="btn-group" >
-            <button>Home</button>
-            <button>My Quizzes</button>
-            <button>Profile</button>
-        </div>
-    );
+return (
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+   <a class="navbar-brand" href="#">QuizMI</a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+   <span class="navbar-toggler-icon"></span>
+   </button>
+   <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+         <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+         </li>
+         <li class="nav-item">
+            <a class="nav-link" href="#">Quiz Control</a>
+         </li>
+         <li class="nav-item">
+            <div class="dropdown">
+               <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               Profile
+               </button>
+               <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="#">My Profile</a>
+                  <a class="dropdown-item" href="#">Switch Account</a>
+                  <a class="dropdown-item" href="#">Logout</a>
+               </div>
+            </div>
+         </li>
+      </ul>
+   </div>
+</nav>
+);
 }
 export default HeaderInstructor;
