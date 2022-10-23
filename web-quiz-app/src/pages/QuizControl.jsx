c44a33f75699b33e12ab6057aad8507fca36da45
matthewkwsiu <22455036+matthewkwsiu@users.noreply.github.com>
2022-10-22 21:04:35 -0400

    Feature add QuizControl page (#4)
    * Add QuizControl page
    
    * rename filename
    
    * add newline and adjust button dimensions

---
+++ web-quiz-app/src/pages/QuizControl.jsx
@@ -0,0 +1,20 @@
+import HeaderInstructor from "./components/HeaderInstructor";
+
+function QuizControl() {
+  return (
+    <div>
+    <HeaderInstructor></HeaderInstructor>
+      <button onClick={generateQuizButton}>New Quiz</button>
+      <div id="button-holder"></div>
+    </div>
+  );
+}
+
+function generateQuizButton(){
+  var btn = document.createElement("button");        
+  var t = document.createTextNode("button text");       
+  btn.appendChild(t);                                
+  document.getElementById('button-holder').appendChild(btn);    
+}
+
+export default QuizControl;
