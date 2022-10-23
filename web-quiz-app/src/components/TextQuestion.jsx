7ecf6c8f9522967c31f5daf14d6bf2a0c716bd06
Itzaek <isaac1937@gmail.com>
2022-10-22 20:58:03 -0400

    Added Text Question component

---
+++ web-quiz-app/src/components/TextQuestion.jsx
@@ -0,0 +1,19 @@
+import './css/TextQuestion.css'
+function TextQuestion() {
+return(
+<form>
+   <div class="form-group">
+      <label for="formGroupExampleInput">Question Title</label>
+      <input type="text" class="form-control" id="formGroupExampleInput" placeholder="What is Java?"></input>
+   </div>
+   <div class="form-group">
+      <label for="formGroupExampleInput2">Solution</label>
+      <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="A programming language"></input>
+   </div>
+   <div class="submissionButton">
+      <button type="submit" class="btn btn-primary">Done</button>
+   </div>
+</form>
+);
+}
+export default TextQuestion;
