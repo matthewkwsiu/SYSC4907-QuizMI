4fc6ef49e32dc88e7349cc7f0d429af69af4461a
Matthew Siu <22455036+matthewkwsiu@users.noreply.github.com>
2022-10-31 15:07:30 -0400

    remove layout from router

--- web-quiz-app/src/error-page.jsx
+++ web-quiz-app/src/error-page.jsx
@@ -2,8 +2,7 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p>An unexpected error has occurred.</p>
     </div>
   );
 }
--- web-quiz-app/src/index.js
+++ web-quiz-app/src/index.js
@@ -5,19 +5,15 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 import ErrorPage from './error-page';
-import Layout from './pages/Layout';
 import QuizControl from './pages/QuizControl';
 
 export default function Example() {
   return (
     <BrowserRouter>
       <Routes>
-
-        <Route path="/" element={<Layout />}>
-          <Route index element={<App />} />
-          <Route path="quizControl" element={<QuizControl/>} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
+        <Route index element={<App />} />
+        <Route path="quizControl" element={<QuizControl/>} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
@@ -25,8 +21,3 @@ export default function Example() {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Example />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
