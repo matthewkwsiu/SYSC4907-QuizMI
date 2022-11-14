0d13345ba08393db88775da2e76b5a8a38de71b8
Matthew Siu <22455036+matthewkwsiu@users.noreply.github.com>
2022-11-14 14:25:45 -0500

    add more columns to models

--- webquizappbackend/quizzes/models.py
+++ webquizappbackend/quizzes/models.py
@@ -2,8 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+class Instructor(models.Model):
+    # quizzes = models.Quiz()
+
+class Student(models.Model):
+    # quizzes_attempted = models.Quiz()
+
+class Quiz(models.Model):
+    quiz_name = models.CharField()
+    course_name = models.CharField()
+    # quiz_owner = models.Instructor()
+    active_status = models.IntegerField(default=0)
+    feedback_status = models.CharField()
+    # questions = models.Question()
+
 class Question(models.Model):
     question_data = models.IntegerField(default=0)
     question_text = models.CharField(max_length=200)
     question_total_marks = models.IntegerField(default=1)
 
+class Response(models.Model):
+    attempt_number = models.IntegerField()
+    response_data = models.Charfield()
+    grade_acheived = models.IntegerField()
