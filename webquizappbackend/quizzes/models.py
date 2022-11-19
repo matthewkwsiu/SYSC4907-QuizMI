e0d1a589140e416ae1c7c62ad1e27c63df0c2c0a
Matthew Siu <22455036+matthewkwsiu@users.noreply.github.com>
2022-11-19 13:46:30 -0500

    add columns to database

--- webquizappbackend/quizzes/models.py
+++ webquizappbackend/quizzes/models.py
@@ -3,27 +3,36 @@ from django.db import models
 # Create your models here.
 
 class Instructor(models.Model):
-    # quizzes = models.Quiz()
     instructor_name = models.CharField(max_length=200)
 
 class Student(models.Model):
-    # quizzes_attempted = models.Quiz()
     student_name = models.CharField(max_length=200)
 
 class Quiz(models.Model):
+    instructor = models.ForeignKey(
+        'Instructor',
+        on_delete=models.CASCADE,
+    )
     quiz_name = models.CharField(max_length=200)
     course_name = models.CharField(max_length=200)
-    # quiz_owner = models.Instructor()
     active_status = models.IntegerField(default=0)
     feedback_status = models.CharField(max_length=200)
-    # questions = models.Question()
 
 class Question(models.Model):
+    quiz = models.ForeignKey(
+        'Quiz',
+        on_delete=models.CASCADE,
+    )
     question_data = models.IntegerField(default=0)
     question_text = models.CharField(max_length=200)
     question_total_marks = models.IntegerField(default=1)
 
 class Response(models.Model):
+    student = models.ForeignKey(
+        'Student',
+        on_delete=models.CASCADE,
+    )
     attempt_number = models.IntegerField()
     response_data = models.CharField(max_length=200)
     grade_acheived = models.IntegerField()
+
