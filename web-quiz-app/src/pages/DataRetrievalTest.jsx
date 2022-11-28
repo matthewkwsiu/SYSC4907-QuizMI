41ea9d2ecac78d2c5588794abf41efd36e6d0486
Itzaek <isaac1937@gmail.com>
2022-11-28 15:57:36 -0500

    update data with useEffect

--- web-quiz-app/src/pages/DataRetrievalTest.jsx
+++ web-quiz-app/src/pages/DataRetrievalTest.jsx
@@ -5,35 +5,34 @@ import {useEffect, useState} from "react";
 
 function DataRetrievalTest() {
 	const [instructors, setNewInstructors] = useState([])
+	
+	function retrieveInstructorList() {
+	QuizDataService.getAllInstructors()
+      .then(response => {
+        setNewInstructors(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+	}
+	
+	useEffect(() => {
+		retrieveInstructorList();
+	}, []);
 	return(
 	<>
 	<div>
 		<button type='button' class="btn btn-link" onClick={() => retrieveInstructorList()}>Retrieve Instructors</button>
-		</div>
-		</>
+	</div>
+	{instructors.map((instructors, index) => (
+                    // Setting "index" as key because name and age can be repeated, It will be better if you assign uniqe id as key
+                    <li key={index}>
+                        <span>id: {instructors.id}</span>{" "}
+                        <span>instructor_name: {instructors.instructor_name}</span>
+                    </li>
+                ))}
+	</>
 	);
-
-	function retrieveInstructorList() {		
-		QuizDataService.getAllInstructors()
-		.then(response => {
-				setNewInstructors(response.data);
-				console.log(response.data);
-		})
-		.catch(e => {
-			console.log(e);
-		}).finally(()=>{
-			console.log(instructors);
-		console.log(instructors[0].id);
-		console.log(instructors[0].instructor_name);
-			
-		});
-
-	}
-	function printSomething(){
-		console.log(instructors);
-		console.log(instructors[0].id);
-		console.log(instructors[0].instructor_name);
-	}
 	
 }
 
