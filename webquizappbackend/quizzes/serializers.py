8fd613ce40cbc76b836716f7164d85637e710631
matthewkwsiu <22455036+matthewkwsiu@users.noreply.github.com>
2022-12-28 10:38:38 -0500

    Add REST APIs for Quizzes (#21)
    * prototype to add post
    
    * add test file
    
    * fix typo
    
    * fix small error
    
    * add ability to get or delete a specific student
    
    * remove unused test class
    
    * initial commit for adding quiz REST APIs
    
    * update GET and DELETE apis
    
    * allow quizzes to POST
    
    * update APIs
    
    * whitepsace

--- webquizappbackend/quizzes/serializers.py
+++ webquizappbackend/quizzes/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from quizzes.models import Instructor
 from quizzes.models import Student
+from quizzes.models import Quiz
 
 class InstructorSerializer(serializers.ModelSerializer):
     
@@ -12,4 +13,12 @@ class StudentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Student
-        fields = ('id','student_name',)
+        fields = ('id','student_name')
+
+class QuizSerializer(serializers.ModelSerializer):
+    instructor_id = serializers.IntegerField(style={'intput_type':'hidden'})
+    
+    class Meta:
+        model = Quiz
+        fields = ('id', 'quiz_name', 'course_name', 'active_status', 'feedback_status', 'instructor_id')
+    
--- webquizappbackend/quizzes/urls.py
+++ webquizappbackend/quizzes/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('instructors', views.instructor_list),
     path('students', views.student_list),
     path('students/<int:pk>', views.student_detail),
+    path('quizzes', views.quiz_list), 
+    path('quizzes/<int:pk>', views.quiz_detail), 
+    path('quizzes/instructor/<int:instructorId>', views.quiz_list_owned_by_instructor),
 ]
