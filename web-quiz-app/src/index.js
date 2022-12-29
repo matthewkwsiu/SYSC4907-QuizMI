6bfbd522382686da037ffcff8feda46f63e3da3f
Matthew Siu <22455036+matthewkwsiu@users.noreply.github.com>
2022-12-29 16:54:12 -0500

    remove files that were moved to other PRs'

--- web-quiz-app/src/index.js
+++ web-quiz-app/src/index.js
@@ -8,9 +8,6 @@ import App from './App';
 import ErrorPage from './error-page';
 import QuizControl from './pages/QuizControl';
 import DataRetrievalTest from './pages/TestPages/DataRetrievalTest';
-import UIPageTest from './pages/TestPages/UIPageTest';
-import JoinQuiz from './pages/JoinQuiz';
-import QuizAnswer from './pages/QuizAnswer';
 
 export default function Example() {
   return (
@@ -19,9 +16,6 @@ export default function Example() {
         <Route index element={<App />} />
         <Route path="quizControl" element={<QuizControl/>} />
         <Route path="instructors" element={<DataRetrievalTest/>} />
-        <Route path="testUI" element={<UIPageTest/>} />
-        <Route path="joinQuiz" element={<JoinQuiz/>} />
-        <Route path="testQuizAnswer" element={<QuizAnswer/>} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
