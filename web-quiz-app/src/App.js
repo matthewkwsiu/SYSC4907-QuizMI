086f39f1808af99bb2f6be58f5ddb9571d9cb8d7
Matthew Siu <22455036+matthewkwsiu@users.noreply.github.com>
2022-12-29 16:31:46 -0500

    initial commit for updating homepage

--- web-quiz-app/src/App.js
+++ web-quiz-app/src/App.js
@@ -11,20 +11,7 @@ function App() {
 	    <HeaderInstructor></HeaderInstructor>
 	  </div>
       
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Welcome to the QuizMI Home Page!</label>
       
     </div>
   );
