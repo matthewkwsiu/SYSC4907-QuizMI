3f2ba343b256da8f68647df959cd7c0f1754c7ec
Matthew Siu <22455036+matthewkwsiu@users.noreply.github.com>
2022-12-30 10:54:16 -0500

    update message

--- web-quiz-app/src/App.js
+++ web-quiz-app/src/App.js
@@ -14,11 +14,11 @@ function App() {
                 <p>Welcome to the QuizMI Home Page!</p>
                 <p>
                     QuizMI is an web app with the goal of assisting instructors in teaching
-                    students. The app allows interactive educational quizzes to be created,
-                    which can be made up of questions with multiple formats, such as text,
-                    numerical, multiple choice, and multiple select questions. These quizzes
-                    can be helpful for improving engagement in classses and provide real-time
-                    feedback on student understanding.
+                    students by providing a tool to create interactive quizzes.
+                </p>
+                <p>
+                    These quizzes can be helpful for improving engagement in classses and
+                    provide real-time feedback on student understanding.
                 </p>
             </header>
         </div>
