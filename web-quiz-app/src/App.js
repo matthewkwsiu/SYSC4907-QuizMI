760afd8de57a1b3ec2028039e3e8de13471921d5
Matthew Siu <22455036+matthewkwsiu@users.noreply.github.com>
2022-12-30 10:50:42 -0500

    adjust home page

--- web-quiz-app/src/App.js
+++ web-quiz-app/src/App.js
@@ -1,20 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import HeaderInstructor from "./components/HeaderInstructor";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    
-    <div className="App">
-	  <div className="Header">
-	    <HeaderInstructor></HeaderInstructor>
-	  </div>
-      
-      <label>Welcome to the QuizMI Home Page!</label>
-      
-    </div>
-  );
+    document.body.style = 'background: #282c34;';
+    return (
+        <div className="App" style={{ background: "black" }}>
+            <div className="Header">
+                <HeaderInstructor></HeaderInstructor>
+            </div>
+            <header className="App-header">
+                <h1>QuizMI</h1>
+                <p>Welcome to the QuizMI Home Page!</p>
+                <p>
+                    QuizMI is an web app with the goal of assisting instructors in teaching
+                    students. The app allows interactive educational quizzes to be created,
+                    which can be made up of questions with multiple formats, such as text,
+                    numerical, multiple choice, and multiple select questions. These quizzes
+                    can be helpful for improving engagement in classses and provide real-time
+                    feedback on student understanding.
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
