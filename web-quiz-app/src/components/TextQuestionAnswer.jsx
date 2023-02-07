fa17a1dce3d566049c0bd31d00966c39ae230227
Matthew Siu <22455036+matthewkwsiu@users.noreply.github.com>
2023-02-06 21:17:37 -0500

    add all button types

--- web-quiz-app/src/components/TextQuestionAnswer.jsx
+++ web-quiz-app/src/components/TextQuestionAnswer.jsx
@@ -5,26 +5,21 @@ class TextQuestionAnswer extends React.Component {
     constructor(props){
         super(props);
     }
-// function TextQuestionAnswer(props) {
+
     render(){
         return (
             <div>
                 <div class="form-group">
-                    <label for="formGroupExampleInput">{this.getQuestion(this.props)}</label>
+                    <label for="formGroupExampleInput">{this.getQuestionText(this.props)}</label>
                     <input type="text" class="form-control" id="formGroupExampleInput" placeholder=""></input>
-                </div>
-                <div class="submitAnswer">
-                    <button type="submit" class="btn btn-primary">Done</button>
                 </div>
             </div>
         );
     }
-    getQuestion(props) {
+
+    getQuestionText(props) {
         return props.text;
     }
-    
 }
-
-
 
 export default TextQuestionAnswer;
