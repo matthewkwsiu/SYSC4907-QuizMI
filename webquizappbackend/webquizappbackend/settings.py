3449fc9d8a562aecb4e2a47216d18f5aace59503
Itzaek <isaac1937@gmail.com>
2023-03-12 20:50:38 -0400

    Modifying to test

--- webquizappbackend/webquizappbackend/settings.py
+++ webquizappbackend/webquizappbackend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quizzes.apps.QuizzesConfig',
+    'apps.QuizzesConfig',
     'rest_framework',
     'corsheaders',
 
